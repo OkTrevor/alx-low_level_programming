648f7cc55e7bf35edf06e180a31228696efc7db7
OkTrevor <117733257+OkTrevor@users.noreply.github.com>
2022-12-15 23:42:14 +0100

    more loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Check for upper case letters
+ * @c: interger to be placed
+ * Return: (1) for uppercase and (0) otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
